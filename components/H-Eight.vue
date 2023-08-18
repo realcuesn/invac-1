124f834d30885dcaff8cc4bcdad5f34fc2194e8c
realcuesn <129620236+realcuesn@users.noreply.github.com>
2023-08-18 17:57:05 +0530

    update

--- components/H-Eight.vue
+++ components/H-Eight.vue
@@ -6,7 +6,8 @@
         <!-- About InVac Section -->
         <div class="w-full border-b-2 border-white pb-4 border-opacity-20">
             <div @click="triggerAbout = !triggerAbout" class="flex cursor-pointer items-end justify-between">
-                <h2 class="text-lg sm:text-2xl lg:text-2xl 2xl:text-3xl  font-semibold">About InVac
+                <h2 class="text-lg sm:text-2xl lg:text-2xl 2xl:text-3xl  font-semibold">How many inlet valves are needed
+                    approximately for a single-family house?
                 </h2>
                 <img src="@/assets/icons/chevron-down.svg"
                     class="h-8 lg:h-8 2xl:h-9 3xl:h-10 mb-2 transition-all duration-1000"
@@ -18,8 +19,8 @@
         <!-- Product Features Section -->
         <div class="w-full border-b-2 border-white pb-4 border-opacity-20">
             <div @click="triggerFeatures = !triggerFeatures" class="flex cursor-pointer items-end justify-between">
-                <h2 class="text-lg sm:text-2xl lg:text-2xl 2xl:text-3xl  font-semibold">Product
-                    Features</h2>
+                <h2 class="text-lg sm:text-2xl lg:text-2xl 2xl:text-3xl  font-semibold">Is suction power consistent
+                    throughout the whole building?</h2>
                 <img src="@/assets/icons/chevron-down.svg"
                     class="h-8 lg:h-8 2xl:h-9 3xl:h-10 mb-2 transition-all duration-1000"
                     :class="{ 'rotate-90': triggerFeatures }" alt="">
@@ -30,8 +31,7 @@
         <!-- Maintenance and Care Section -->
         <div class="w-full border-b-2 border-white pb-4 border-opacity-20">
             <div @click="triggerMaintenance = !triggerMaintenance" class="flex cursor-pointer items-end justify-between">
-                <h2 class="text-lg sm:text-2xl lg:text-2xl 2xl:text-3xl  font-semibold">Maintenance and
-                    Care</h2>
+                <h2 class="text-lg sm:text-2xl lg:text-2xl 2xl:text-3xl  font-semibold">Is there a risk of blockage?</h2>
                 <img src="@/assets/icons/chevron-down.svg"
                     class="h-8 lg:h-8 2xl:h-9 3xl:h-10 mb-2 transition-all duration-1000"
                     :class="{ 'rotate-90': triggerMaintenance }" alt="">
@@ -42,8 +42,8 @@
         <!-- Customer Support Section -->
         <div class="w-full border-b-2 border-white pb-4 border-opacity-20">
             <div @click="triggerSupport = !triggerSupport" class="flex cursor-pointer items-end justify-between">
-                <h2 class="text-lg sm:text-2xl lg:text-2xl 2xl:text-3xl  font-semibold">Customer
-                    Support</h2>
+                <h2 class="text-lg sm:text-2xl lg:text-2xl 2xl:text-3xl  font-semibold">Why you should not install a Invac
+                    central vacuum cleaner in the attic.</h2>
                 <img src="@/assets/icons/chevron-down.svg"
                     class="h-8 lg:h-8 2xl:h-9 3xl:h-10 mb-2 transition-all duration-1000"
                     :class="{ 'rotate-90': triggerSupport }" alt="">
@@ -54,7 +54,8 @@
         <!-- New Section: Cleaning Tips -->
         <div class="w-full border-b-2 border-white pb-4 border-opacity-20">
             <div @click="triggerTips = !triggerTips" class="flex cursor-pointer items-end justify-between">
-                <h2 class="text-lg sm:text-2xl lg:text-2xl 2xl:text-3xl  font-semibold">Cleaning Tips
+                <h2 class="text-lg sm:text-2xl lg:text-2xl 2xl:text-3xl  font-semibold">Can a Invac system be installed in a
+                    present home?
                 </h2>
                 <img src="@/assets/icons/chevron-down.svg"
                     class="h-8 lg:h-8 2xl:h-9 3xl:h-10 mb-2 transition-all duration-1000"
@@ -70,18 +71,20 @@
 import { ref } from 'vue';
 
 const triggerAbout = ref(false);
-const aboutContent = ref(`Discover the story behind InVac and our commitment to providing exceptional vacuum solutions.`);
+const aboutContent = ref(`Our motto: As many as necessary and as few as possible. In order to reach all areas from basement to attic, the inlet valves should be placed appropriate to the architectual circumstances of the building. The suction hoses have, depending on the length, a radius of action of 6 to 12 meters.
+A single-family home with a living space of about 150 square meters requires normally 5 to 7 inlet valves.
+We recommend to install an inlet valve at the entrance of the garage in order to vacuum the car comfortably and thoroughly. With a pipe valve set it is possible to vacuum the room in which the central vacuum unit is located.`);
 
 const triggerFeatures = ref(false);
-const featuresContent = ref(`Explore the advanced features of our vacuum products, designed to make cleaning effortless and effective.`);
+const featuresContent = ref(`Distances up to 100 m are no problem. A properly installed Invac central vacuum system offers the same powerful cleaning performance at every inlet valve and won‘t lose suction over time. Invac offers units with various motor strenghts, so every building has a central vacuum cleaner with optimal performance.`);
 
 const triggerMaintenance = ref(false);
-const maintenanceContent = ref(`Learn how to properly maintain and care for your InVac vacuum to ensure optimal performance and longevity.`);
+const maintenanceContent = ref(`No, provided that the pipes are properly installed. A so called safety bend is located behind each inlet valve. This is a short 87° bend that prevents bigger edgy objects from entering into the piping system. If, despite of this, a blockage occurs (e.g. vacuumed cloth fabrics), there are a few easy actions to solve the problem.`);
 
 const triggerSupport = ref(false);
-const supportContent = ref(`Our dedicated customer support team is here to assist you with any inquiries, troubleshooting, or product information.`);
+const supportContent = ref(`Due to gravity, large and heavy particles can not be sucked up several meters. There is a risk that the particles remain inside the piping system and fall to the deepest inlet valve. For this reason we suggest to install the central vacuum cleaner at the deepest point in your home.`);
 
 const triggerTips = ref(false);
-const tipsContent = ref(`Get valuable cleaning tips and techniques to achieve the best results with your InVac vacuum, tailored to different cleaning needs.`);
+const tipsContent = ref(`Yes! Virtually any home, from new to decades old, can enjoy the cleaning power of a Invac system. The easiest way is to install pipelines into an inoperative chimney, supply shaft or open stairwell.There are always different installation possibilities. Everything is do-able. Your Invac Agent will be pleased to help you with your installation options.`);
 </script>
   
--- components/H-Seventh.vue
+++ components/H-Seventh.vue
@@ -6,70 +6,85 @@
         </div>
 
         <div class="w-full hidden lg:grid grid-cols-2 px-10 lg:px-20  gap-x-10 2xl:gap-x-14 mt-20">
-            <div class="w-full cursor-pointer">
-                <div class="h-[calc(60vh+2.5rem)] w-full rounded-md bg-neutral-800"></div>
+            <NuxtLink to="/blogs/blog/VG4RQtmR74C8Nju5OliZ" class="w-full cursor-pointer">
+                <div class="h-[calc(60vh+2.5rem)] w-full rounded-md bg-neutral-800">
+                    <NuxtImg format="webp" class="h-full w-full rounded-md object-cover" src="/images/blog/Asthama.jpg">
+                    </NuxtImg>
+                </div>
                 <div class="text-3xl mt-10">
-                    Lorem ipsum dolor sit amet consectetur
-                    Lorem ipsum dolor sit amet consectetur
+                    5 Signs that Signal an Upgrade to a New Central Vacuum System
                 </div>
 
                 <div class="text-neutral-600 mt-10 font-semibold text-xl">View More</div>
-            </div>
+            </NuxtLink>
             <div class="w-full">
                 <div class="space-y-10">
-                    <NuxtLink class="grid grid-cols-2">
-                        <div class="h-[30vh] w-full bg-neutral-800 rounded-md"></div>
+                    <NuxtLink to="/blogs/blog/Lh7VbIj8NIXsbsaePmgl" class="grid grid-cols-2">
+                        <div class="h-[30vh] w-full bg-neutral-800 rounded-md">
+                            <NuxtImg format="webp" class="h-full w-full rounded-md object-cover"
+                                src="/images/blog/Retain Air Freshness and Efficiency with an Industrial Central Vacuum System-01.png">
+                            </NuxtImg>
+                        </div>
                         <div class="h-[30vh] flex flex-col justify-evenly overflow-clip p-5">
                             <div class="w-full text-xl font-semibold">
-                                Lorem ipsum dolor sit amet consectetur
+                                Retain Air Freshness and Efficiency with an Industrial Central Vacuum System
                             </div>
 
                             <div class="text-neutral-600 font-semibold text-lg">View More</div>
                         </div>
                     </NuxtLink>
 
-                    <div class="grid grid-cols-2">
-                        <div class="h-[30vh] w-full bg-neutral-800 rounded-md"></div>
+                    <NuxtLink to="/blogs/blog/4DDBdXgimurIP5IGWsV3" class="grid grid-cols-2">
+                        <div class="h-[30vh] w-full bg-neutral-800 rounded-md">
+                            <NuxtImg format="webp" class="h-full w-full rounded-md object-cover"
+                                src="/images/blog/Tips to Make Home Cleaning Effortless.jpg">
+                            </NuxtImg>
+                        </div>
                         <div class="h-[30vh] flex flex-col justify-evenly overflow-clip p-5">
                             <div class="w-full text-xl font-semibold">
-                                Lorem ipsum dolor sit amet consectetur
+                                Tips to Make Home Cleaning Effortless with Central Vacuum Cleaners
                             </div>
 
                             <div class="text-neutral-600 font-semibold text-lg">View More</div>
                         </div>
-                    </div>
+                    </NuxtLink>
                 </div>
             </div>
         </div>
 
         <div class="w-full overflow-x-clip">
             <div class="w-full flex overflow-x-auto gap-x-7 lg:hidden mt-10 pl-10">
-                <div class="w-[85%] min-w-[85%]">
-                    <div class="w-full h-64 rounded-md bg-neutral-800"></div>
+                <NuxtLink to="/blogs/blog/VG4RQtmR74C8Nju5OliZ" class="w-[85%] min-w-[85%]">
+                    <div class="w-full h-64 rounded-md bg-neutral-800">
+                        <NuxtImg format="webp" class="h-full w-full rounded-md object-cover" src="/images/blog/Asthama.jpg">
+                        </NuxtImg>
+                    </div>
+                    <h3 class="mt-7 text-xl"> 5 Signs that Signal an Upgrade to a New Central Vacuum System</h3>
+                    <div class="text-neutral-600 font-semibold text-lg mt-5">View More</div>
+                </NuxtLink>
+
+                <NuxtLink to="/blogs/blog/Lh7VbIj8NIXsbsaePmgl" class="w-[85%] min-w-[85%]">
+                    <div class="w-full h-64 rounded-md bg-neutral-800">
+                        <NuxtImg format="webp" class="h-full w-full rounded-md object-cover"
+                            src="/images/blog/Retain Air Freshness and Efficiency with an Industrial Central Vacuum System-01.png">
+                        </NuxtImg>
+                    </div>
+                    <h3 class="mt-7 text-xl"> Retain Air Freshness and Efficiency with an Industrial Central Vacuum System
+                    </h3>
+                    <div class="text-neutral-600 font-semibold text-lg mt-5">View More</div>
+                </NuxtLink>
+
+
+                <NuxtLink to="/blogs/blog/4DDBdXgimurIP5IGWsV3" class="w-[85%] min-w-[85%]">
+                    <div class="w-full h-64 rounded-md bg-neutral-800">
+                        <NuxtImg format="webp" class="h-full w-full rounded-md object-cover"
+                            src="/images/blog/Tips to Make Home Cleaning Effortless.jpg">
+                        </NuxtImg>
+                    </div>
                     <h3 class="mt-7 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
                     <div class="text-neutral-600 font-semibold text-lg mt-5">View More</div>
-                </div>
+                </NuxtLink>
 
-                <div class="w-[85%] min-w-[85%]">
-                    <div class="w-full h-64 rounded-md bg-neutral-800"></div>
-                    <h3 class="mt-7 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                    <div class="text-neutral-600 font-semibold text-lg mt-5">View More</div>
-                </div>
-
-
-                <div class="w-[85%] min-w-[85%]">
-                    <div class="w-full h-64 rounded-md bg-neutral-800"></div>
-                    <h3 class="mt-7 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                    <div class="text-neutral-600 font-semibold text-lg mt-5">View More</div>
-                </div>
-
-                <div class="w-[85%] min-w-[85%]">
-                    <div class="w-full h-64 rounded-md bg-neutral-800"></div>
-                    <h3 class="mt-7 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                    <div class="text-neutral-600 font-semibold text-lg mt-5">View More</div>
-                </div>
-
-               
             </div>
         </div>
 
